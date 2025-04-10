51f527ce9234be164bf1c681c6c27f4e1fd81ba8
GuilhermeRaphael <guigaorapha07@gmail.com>
2025-04-10 19:40:40 -0300

    Corrigido a main

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,25 @@
+import Models.Cd;
+import Models.Dvd;
 import Models.Livro;
+import Models.Produto;
 
 public class Principal {
     public static void main(String[] args) {
 
         Livro livro = new Livro();
-        livro.setPrecoVenda(100);
+        livro.setPrecoCusto(60);
         livro.calcularPrecoVenda();
+        System.out.println(livro.getPrecoVenda());
+
+        Dvd dvd = new Dvd();
+        dvd.setPrecoCusto(80);
+        dvd.calcularPrecoVenda();
+        System.out.println(dvd.getPrecoVenda());
+
+        Cd cd = new Cd();
+        cd.setPrecoCusto(100);
+        cd.calcularPrecoVenda();
+        System.out.println(cd.getPrecoVenda());
 
 
     }
